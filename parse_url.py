5b11f055274a8c912c1e267c213537fbc4a9e60a
keqiangtong <keqiangtong@gmail.com>
2019-02-11 18:26:45 -0800

    spider 2019/2/12 xihong

--- parse_url.py
+++ parse_url.py
@@ -6,12 +6,14 @@ from retrying import retry
 headers = {
             "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X)\
              AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372\
-             Safari/604.1"
+             Safari/604.1",
+
+
         }
 
-@retry(stop_max_attempt_number = 4)
+@retry(stop_max_attempt_number = 20)
 def _parse_url(url):
-
+    print('*'*10)
     response = requests.get(url,headers=headers)
     assert response.status_code == 200
     return response.content.decode()
@@ -27,7 +29,7 @@ def parse_url(url):
 
 
 if __name__ == '__main__':
-    rst = parse_url('http://www.baidu.com')
+    rst = parse_url('https://m.douban.com/rexxar/api/v2/subject_collection/movie_showing/items?start=0&count=18&loc_id=108288')
     print(rst)
 
 
